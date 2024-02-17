7d54acf7ab76891b32c8540f89020aaae5b514c8
AndyPythonCode <andyarciniegas24@gmail.com>
2024-02-17 19:55:04 -0400

    d

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import uvicorn
 from fastapi import FastAPI
-
 
 app = FastAPI(docs_url="/")
 
@@ -7,3 +7,7 @@ app = FastAPI(docs_url="/")
 @app.get("/hola")
 async def home():
     return "hola"
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
